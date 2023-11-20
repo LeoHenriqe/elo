0d5d2f312dfab27c5330fc1820a977a4e15266d0
Leonardo Henrique <90967601+LeoHenriqe@users.noreply.github.com>
2023-11-20 14:27:34 -0300

    up

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-$(".menu-toggle").on('click', function() {
-    $(this).toggleClass("on");
-    $('.menu-section').toggleClass("on");
-    $("nav ul").toggleClass('hidden');
-  });
+$(".menu-toggle").on('click', function () {
+  $(this).toggleClass("on");
+  $('.menu-section').toggleClass("on");
+  $("nav ul").toggleClass('hidden');
+});
